cbcc4e5e29e1052450b68d31d280daeab2e889ab
lianai911 <lianai911@qq.com>
2018-03-01 20:01:20 +0800

    1. 生成器

--- first/generator.py
+++ first/generator.py
@@ -4,12 +4,38 @@
 # for i in g:
 #     print(i)
 
+# def fib(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         print(b)
+#         a, b = b, a + b
+#         n = n + 1
+#     return 'done'
+#
+# fib(6)
+
 def fib(max):
     n, a, b = 0, 0, 1
     while n < max:
-        print(b)
+        yield b
         a, b = b, a + b
         n = n + 1
     return 'done'
 
-fib(6)
+print(fib(6))
+
+for i in fib(6):
+    print(i)
+
+def triangles():
+    N = [1]
+    while True:
+        yield N
+        N = [1] + [N[i] + N[i+1] for i in range(len(N)-1)] +[1]
+
+n = 0
+for t in triangles():
+    print(t)
+    n=n+1
+    if n == 10:
+        break
